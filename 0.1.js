92cb776e80099ca88e6b4aa8f27b4271a06a4b8a
pkarrasco <pcarrascoortega@gmail.com>
2022-11-30 21:30:05 +0100

    Init

---
+++ 0.1.js
@@ -0,0 +1,14 @@
+const { Console } = require("console-mpds");
+const console = new Console();
+
+/*
+Introduce el numerador de la fracción: 2
+Introduce el denominador de la fracción: 3
+Introduce un exponente: 4
+La fracción 2/3 elevado a 4 es la fracción 16/81
+*/
+
+const numerator = console.readNumber(`Introduce el numerador de la fracción:`)
+const denominator = console.readNumber(`Introduce el denominador de la fracción:`)
+const exponent = console.readNumber(`Introduce un exponente:`)
+console.writeln(`La fracción ${numerator}/${denominator} elevado a 4 es la fracción es la fracción ${numerator ** exponent}/${denominator ** exponent}`)
